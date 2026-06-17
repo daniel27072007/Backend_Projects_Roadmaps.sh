37c3258a013edead91db0c5a60354dcaa2a2a93f
daniel27072007 <daniel.belculfine@gmail.com>
2026-06-17 14:37:28 -0300

    Learning

---
+++ Learning Projects/Tutorial3/Functions/math.js
@@ -0,0 +1,10 @@
+
+function sum(a, b){
+    return a + b;
+}
+
+function sub(a, b){
+    return a - b;
+}
+
+export { sum ,sub }
---
+++ Learning Projects/Tutorial3/index.js
@@ -0,0 +1,5 @@
+import { sum,sub } from './Functions/math.js';
+import "./Functions/hello.js"
+
+console.log(sum(4,5));
+console.log(sub(10,7));
