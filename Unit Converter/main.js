43ca89619e3da8161900ecd6b1a5f90e2a231cad
daniel27072007 <daniel.belculfine@gmail.com>
2026-07-02 22:34:57 -0300

    Unit Conver Project

--- Unit Converter/main.js
+++ Unit Converter/main.js
@@ -1,33 +1,9 @@
-// import http from 'http';
-// import fs from 'fs'; 
-const lengthForm = document.querySelector(`.lengthForm`);
-const endButton = document.querySelector(`#endButton`);
+console.log("teste")
+const lengthForm = document.querySelector(".lengthForm");
+const btn = document.querySelector("#endButton");
 
-const paramters = new URLSearchParams(window.location.search);
-const unitDigits = Number(paramters.get('unitDigits'));
-const unitConvertFrom = paramters.get('unitConvertFrom');
-const unitConvertTo = paramters.get('unitConvertTo');
-let result = unitDigits; 
-
-endButton.onclick = ()=>{
+btn.addEventListener('click', (event)=>{
     event.preventDefault();
-    lengthForm.classList.add("close");
-};
-
-const lengthConverter = (unitDigits, unitConvertFrom, unitConvertTo)=>{
-    if(!unitDigits || !unitConvertFrom || !unitConvertTo){
-        return;   
-    }
-
-    if(unitConvertFrom === "Centimetros" && unitConvertTo === "Metros"){
-        result = unitDigits / 100;
-    }
-    else if(unitConvertFrom === "Metros" && unitConvertTo === "Centimetros"){
-        result = unitDigits * 100;
-    }
-
-    console.log(`Result of your calculation`)
-    console.log(`${unitDigits} ${unitConvertFrom} => ${result} ${unitConvertTo}`)
-};
-
-lengthConverter(unitDigits, unitConvertFrom, unitConvertTo);
+    console.log("formulario recebido");
+    document.body.style.backgroundColor = "red";
+});
