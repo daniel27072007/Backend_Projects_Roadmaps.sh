f1c8065df333f9993a18bdd4b427786e3345d575
daniel27072007 <daniel.belculfine@gmail.com>
2026-07-04 18:32:03 -0300

    Unit Converter Project

--- Unit Converter/main.js
+++ Unit Converter/main.js
@@ -1,9 +1,36 @@
-console.log("teste")
 const lengthForm = document.querySelector(".lengthForm");
-const btn = document.querySelector("#endButton");
+const unitDigits = document.querySelector("#unitDigits");
+const unitConvertFrom = document.querySelector("#unitConvertFrom");
+const unitConvertTo = document.querySelector("#unitConvertTo");
+const btnForm = document.querySelector("#endButton");
 
-btn.addEventListener('click', (event)=>{
+const resultBox = document.querySelector(".resultBox");
+const resultText = document.querySelector("#resultText");
+const btnResult = document.querySelector("#resetButton");
+
+lengthForm.classList.remove("close");
+resultBox.classList.add("close");
+
+const lengthConverter = (unitDigits, firstUnit, secondUnit) => {
+    return unitDigits;
+}
+
+lengthForm.addEventListener('submit', (event) => {
     event.preventDefault();
-    console.log("formulario recebido");
-    document.body.style.backgroundColor = "red";
+
+    const formDigits = unitDigits.value;
+    const formUnit1 = unitConvertFrom.value;
+    const formUnit2 = unitConvertTo.value;
+    const resultDigits = lengthConverter(formDigits, formUnit1, formUnit2);
+
+
+    lengthForm.classList.add("close");
+    resultBox.classList.remove("close");
+
+    document.body.style.backgroundColor = "blue"; 
+    resultText.innerText = `${formDigits} ${formUnit1} = ${resultDigits} ${formUnit2}`;
+});
+
+btnResult.addEventListener('click', (event) => {
+    location.reload();
 });
