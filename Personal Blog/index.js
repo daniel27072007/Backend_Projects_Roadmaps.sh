8770590081364d5fa37cebe590029e9748044bf0
daniel27072007 <daniel.belculfine@gmail.com>
2026-07-10 19:04:52 -0300

    Personal Blog Project

--- Personal Blog/index.js
+++ Personal Blog/index.js
@@ -9,23 +9,57 @@ app.use(express.static('public'));
 app.use(express.urlencoded());
 app.use(express.json());
 
+const basicAutentication = (req, res, next) => {
+    const autenticationHeader = req.headers.authorization; 
+    if(!autenticationHeader){
+        res.setHeader('WWW-Authenticate', 'Basic realm="Paginas Admin"');
+        return res.status(401).send('Autentication Necessary.')
+    }
+    const parts = autenticationHeader.split(' ');
+    const base64String = parts[1]; 
+    const credentialsText = Buffer.from(base64String, 'base64').toString('ascii');
+    const [username, password] = credentialsText.split(':');
+
+    if(username === 'admin' && password === '123'){
+        next();
+    }
+    else{
+        res.setHeader('WWW-Authenticate', 'Basic realm="Paginas Admin"');
+        return res.status(401).send('Username or Password Invalid.')
+    }
+}
+
+const formatDate = (dateString) => {
+    const date = new Date(dateString + "T00:00:00Z")
+    return new Intl.DateTimeFormat('en-US', {
+        month: 'long',
+        day: 'numeric',
+        year: 'numeric',
+        timeZone: 'UTC'
+    }).format(date);
+};
+
 app.get('/admin/:id', (req, res)=>{
     const blogId = req.params.id
     JSONDelete('./data/blogData.json', blogId);;
     res.redirect('/admin');
 });
 
-app.get('/admin', (req, res)=>{
+app.get('/admin', basicAutentication, (req, res)=>{
     const blogData = JSONRead('./data/blogData.json');
     res.render('admin', { blogData });
 });
 
 app.get('/home', (req, res)=>{
-    res.render('home');
+    const blogData = JSONRead('./data/blogData.json');
+    res.render('home', { blogData, formatDate });
 });
 
-app.get('/article', (req, res)=>{
-    res.render('article');
+app.get('/article/:id', (req, res)=>{
+    const articleId = req.params.id
+    const blogData = JSONRead('./data/blogData.json');
+    const articleSearched = blogData.find((element)=> element.id === Number(articleId));
+    res.render('article', { articleSearched, formatDate });
 });
 
 app.get('/edit/:id', (req, res)=>{
@@ -61,5 +95,5 @@ app.post('/add', (req, res)=>{
 
 const PORT = 3000
 app.listen(PORT, ()=>{
-    console.log(`Servidor rodando em http://localhost:${PORT}/admin`);
+    console.log(`Servidor rodando em http://localhost:${PORT}/home`);
 });
