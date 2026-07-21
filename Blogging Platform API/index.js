70811a6787eaa21d10c746f601185f050b6d6b35
daniel27072007 <daniel.belculfine@gmail.com>
2026-07-21 00:02:38 -0300

    Blogging Platform API Project

--- Blogging Platform API/index.js
+++ Blogging Platform API/index.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import { rateLimit } from 'express-rate-limit'
+import mongoose from 'mongoose'
+import 'dotenv/config'
 
-const app = new express();
+const app = express();
 const limiter = rateLimit({
     windowMs: 1000,
     limit: 2,
@@ -11,10 +13,54 @@ const limiter = rateLimit({
 app.use(express.json());
 app.use(limiter);
 
+const mongoUri = process.env.MONGO_URI
+
+mongoose.connect(mongoUri)
+    .then(()=>{
+        console.log('conect with success to the cluster!')
+    })
+    .catch((error)=>{
+        console.error('cannot connect with cluster ', error)
+    });
+
+const schemaPOST = new mongoose.Schema({
+    title: {type: String, required: true},
+    content: {type: String, required: true},
+    category: {type: String, required: true},
+    tags: {
+        type: [String],
+        validate: {
+            validator: function(array){
+                return array && array.length > 0;
+            },
+            message: 'The post need to have at least one tag'
+        }
+    }
+},{
+    timestamps:true
+});
+
+const post = mongoose.model('Post', schemaPOST);
+
 app.post('/post', async (req, res)=>{
     console.log('post rechead');
-    const postData = req.body
-    console.log('postData recived!');
+    try{
+        const newPost = new post(req.body);
+        const savedPost = await newPost.save();
+        res.status(201).json(savedPost);
+        console.log('post saved in the data base!')
+    }
+    catch(error){
+        if(error.name === 'ValidationError'){
+            console.warn('The data send was incomplete:', error.message);
+            return res.status(400).json({
+                error: 'Bad Request',
+                message: error.message
+            });
+        }
+        console.error('Something gone wrong with the server: ', error);
+        res.status(500).json({error: 'Internal server error, failed to save post'})
+    }
 });
 
 const PORT = 3000
